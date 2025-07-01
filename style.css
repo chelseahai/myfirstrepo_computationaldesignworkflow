d09647c7dde18b0fd21c35946af9da9f041e0ae7
chelseahai <jh5138@columbia.edu>
2025-07-01 18:14:37 -0400

    Update style.css

---
+++ style.css
@@ -0,0 +1,16 @@
+// script.js: This file adds interactivity to the website
+
+// Wait until the entire page is loaded
+window.onload = function() {
+    // Get the button element using its ID
+    const button = document.getElementById('clickButton');
+
+    // Get the paragraph element where the message will be displayed
+    const message = document.getElementById('message');
+
+    // Add a click event to the button
+    button.addEventListener('click', function() {
+        // When the button is clicked, show a message
+        message.textContent = 'You clicked the button!';
+    });
+};
