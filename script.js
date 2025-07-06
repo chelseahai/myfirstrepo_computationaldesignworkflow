db331609416dc9c130a3943f046cb2425c75a989
chelseahai <jh5138@columbia.edu>
2025-07-06 11:51:11 -0400

    Deploy gh-pages version

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js: Keeps original function, adds a text emoji to alert
 document.getElementById("clickButton").addEventListener("click", function () {
     alert("You clicked the button! ♡(｡- ω -)");
 });
