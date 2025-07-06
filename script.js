97a7b71a24fe1c3d69264ff795c48c72ec262e1b
chelseahai <jh5138@columbia.edu>
2025-07-06 11:15:55 -0400

    Modify website template through 'vibe coding'

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js: Keeps original function, adds a text emoji to alert
+document.getElementById("clickButton").addEventListener("click", function () {
+    alert("You clicked the button! ♡(｡- ω -)");
+});
